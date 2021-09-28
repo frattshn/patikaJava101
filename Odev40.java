9bc82cdbb363a366bed3670c31fbf02632f7f377
Memduh Fırat Şahin <57902407+frattshn@users.noreply.github.com>
2021-09-28 14:47:58 +0300

    Create Odev40.java

---
+++ Odev40.java
@@ -0,0 +1,40 @@
+/**
+ * Dizideki Tekrar Eden Sayıları Bulan Program
+ */
+
+package patikaJava101;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+	static boolean isFind(int[] arr, int value) {
+		for(int i : arr) {
+			if(i==value) return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		/**
+		 * @author MEMDUH FIRAT 28.09.2021
+		 */	
+				
+		int[] array= {5, 3, 10, -2, 5, -2, 7, 3};
+		int[] duplicateArray= new int[array.length];
+		
+		int startIndex= 0;
+		
+		for(int i=0; i<array.length; i++) {
+			for(int j=0; j<array.length; j++) {
+				if((i!=j) && (array[i]==array[j])) {
+					if(!isFind(duplicateArray, array[i]))
+						duplicateArray[startIndex++]= array[i];
+					//break;
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(duplicateArray));
+	}
+}
